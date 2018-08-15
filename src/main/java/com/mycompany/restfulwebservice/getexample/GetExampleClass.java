422818d59d95f8259b9537b5855cac4c2a92013c
Oğuzhan Mevsim <oguzhanmevsim@gmail.com>
2018-08-15 15:08:14 +0300

    Update GetExampleClass.java

--- src/main/java/com/mycompany/restfulwebservice/getexample/GetExampleClass.java
+++ src/main/java/com/mycompany/restfulwebservice/getexample/GetExampleClass.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.restfulwebservice.getexample;
 
 import com.mycompany.restfulwebservice.pojo.Person;
@@ -18,8 +13,6 @@ import javax.ws.rs.core.MediaType;
  * @author Mevsim
  */
 
-//Web servis olduğunu belirtme
-//getExample altındaki url isteklerini burası karşılayacak
 @Path("/getExample")
 public class GetExampleClass {
     
