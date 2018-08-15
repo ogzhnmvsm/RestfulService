a41379e403d36cc59910105937c6b2ef9aa57c58
Oğuzhan Mevsim <oguzhanmevsim@gmail.com>
2018-08-15 15:08:34 +0300

    Update PostExampleClass.java

--- src/main/java/com/mycompany/restfulwebservice/postExample/PostExampleClass.java
+++ src/main/java/com/mycompany/restfulwebservice/postExample/PostExampleClass.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.restfulwebservice.postExample;
 
 import com.mycompany.restfulwebservice.pojo.Person;
