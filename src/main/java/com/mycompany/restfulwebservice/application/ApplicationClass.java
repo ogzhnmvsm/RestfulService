746fbd03621e5930f8a638e67ebc07f1d8c62d92
Oğuzhan Mevsim <oguzhanmevsim@gmail.com>
2018-08-15 15:06:39 +0300

    Update ApplicationClass.java

--- src/main/java/com/mycompany/restfulwebservice/application/ApplicationClass.java
+++ src/main/java/com/mycompany/restfulwebservice/application/ApplicationClass.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.restfulwebservice.application;
 
 import javax.ws.rs.ApplicationPath;
@@ -13,10 +8,6 @@ import javax.ws.rs.core.Application;
  * @author Mevsim
  */
 
-//Notasyon bazlı servlet tanımlama. Hiç web xml e bulaşmadan bu notation ile
-//resteasy nin çalışma mantığını tanıtmış olduk. Web.xml ile uğraşmayacağız
-//web servis projemizin root path i. 
-//Web servise gelen istekler öncelikle gelecek
 @ApplicationPath("/webservice")
 public class ApplicationClass extends Application{
     
